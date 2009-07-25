7f463712b3a7389fd31280d32da0ff3ca84355a2
Victor Costan <costan@gmail.com>
2009-07-25 08:57:57 -0400

    Push notifications and feedback.

--- lib/imobile/push_notification.rb
+++ lib/imobile/push_notification.rb
@@ -6,6 +6,8 @@
 
 require 'net/http'
 require 'net/https'
+
+require 'socket'
 require 'openssl'
 
 require 'rubygems'
@@ -15,23 +17,86 @@ require 'json'
 # :nodoc: namespace
 module Imobile
 
-# Sends push notifications to iMobile devices.
+# Sends a push notification to an iMobile device.
 #
 # Args:
-#   notification:: ruby Hash indicating the desired notification
-#   certificate_path:: path to the certificate required to talk to APNs
+#   notification:: ruby Hash indicating the desired notification; the hash
+#                  should have an extra key named :device_token, containing
+#                  the binary-encoded (not hexadecimally-encoded) iMobile device
+#                  token, as provided by the UIApplicationDelegate method
+#                  application:didRegisterForRemoteNotificationsWithDeviceToken: 
+#   path_or_certificate:: the certificate required to talk to APNs; this can be
+#                         a path to a .p12 file, a string with the contens of
+#                         the .p12 file, or a previously-read certificate
 #
 # Raises a RuntimeException if Apple's Push Notification service doesn't behave.
-def self.push_notification(notification, certificate_path)
-  AppStoreReceiptValidation.push_notification notification, certificate_path
+def self.push_notification(notification, path_or_certificate)
+  PushNotifications.push_notification notification, path_or_certificate
 end
 
+# Bulk-transmission of push notifications to Apple's service.
+#
+# Args:
+#   path_or_certificate:: see push_notification
+#   notifications:: an array of notification hashes; see push_notification
+# 
+# If the method receives a block, it yields to its block indefinitely. Each
+# time, the block should +next+ a notification or array of notifications to be
+# pushed. The block should +break+ when it's done.  
+def self.push_notifications(path_or_certificate, notifications = [], &block)
+  PushNotifications.push_notifications path_or_certificate, notifications,
+                                       &block
+end
+
+# Reads the available feedback from Apple's Push Notification service.
+#
+# Args:
+#   certificate_or_path:: see Imobile.push_notification
+#
+# The currently provided feedback is the tokens for the devices which rejected
+# notifications. Each piece of feedback is a hash with the following keys:
+#   :device_token:: the device's token, in binary (not hexadecimal) format
+#   :time:: the last time when the device rejected notifications; according to
+#           Apple, the rejection can be discarded if the device sent a
+#           token after this time
+#
+# The method reads all the feedback available from the Push Notification
+# service. If a block is given, each piece of feedback is yielded to the
+# method's block, and the method returns nil. If no block is given, the 
+# method returns an array containing all pieces of feedback.
+def self.push_feedback(path_or_certificate, &block)
+  PushNotifications.push_feedback path_or_certificate, &block
+end
+
+# Checks if a notification is valid for Apple's Push Notification service.
+#
+# Currently, notifications are valid if their JSON encodings don't exceed 256
+# bytes.
+def self.valid_notification?(notification)
+  PushNotifications.encode_notification(notification) ? true : false
+end
+
+# Packs a hexadecimal iMobile device token into binary form.
+def self.pack_hex_device_token(device_token)
+  [device_token.gsub(/\s/, '')].pack('H*')
+end
 
 # Implementation details for push_notification.
 module PushNotifications
+  # Reads an APNs certificate from a string or a file.
+  def self.read_certificate(certificate_blob_or_path)
+    unless certificate_blob_or_path.respond_to? :to_str
+      return certificate_blob_or_path
+    end    
+    begin
+      decode_push_certificate File.read(certificate_blob_or_path)
+    rescue
+      decode_push_certificate certificate_blob_or_path
+    end
+  end
+  
   # Decodes an APNs certificate.
-  def self.read_push_certificate(path)
-    certificate_blob = File.read path
+  def self.decode_push_certificate(certificate_blob)    
     pkcs12 = OpenSSL::PKCS12.new certificate_blob
     
     certificate = pkcs12.certificate
@@ -48,11 +113,148 @@ module PushNotifications
     { :certificate => certificate, :key => key, :server_type => server_type }
   end
   
-  # Real implementation of Imobile.push_notification
-  def self.push_notification(notification, certificate_path)
-    uri = store_uri server_type
+  # Encodes a push notification in a binary string for APNs consumption.
+  #
+  # Returns a string suitable for transmission over an APNs, or nil if the
+  # notification is invalid (i.e. the json encoding exceeds 256 bytes).
+  def self.encode_notification(notification)
+    device_token = notification[:device_token] || ''
+    notification = notification.dup
+    notification.delete :device_token
+    json_notification = notification.to_json
+    return nil if json_notification.length > 256
     
-    process_response issue_request(request(receipt_blob, uri), uri)     
+    ["\0", [device_token.length].pack('n'), device_token,
+     [json_notification.length].pack('n'), json_notification].join
+  end
+    
+  # Creates a socket to an Apple Push Notification Server.
+  #
+  # Args:
+  #   push_certificate:: the APNs client certificate data, obtained by a call to
+  #                      read_certificate
+  #   service:: either :feedback or :push
+  #    
+  # The returned socket is connected and ready for use.
+  def self.apns_socket(push_certificate, service = :push)
+    context = OpenSSL::SSL::SSLContext.new
+    context.cert = push_certificate[:certificate]
+    context.key = push_certificate[:key]
+    
+    server_type = push_certificate[:server_type]
+    raw_socket = TCPSocket.new apns_host(server_type, service),
+                               apns_port(server_type, service)
+    
+    socket = OpenSSL::SSL::SSLSocket.new raw_socket, context
+    # Magic for closing the raw socket when the SSL socket is closed.
+    (class <<socket; self; end).send :define_method, :close do
+      super
+      raw_socket.close
+    end
+    socket.sync = true
+    socket.connect
+  end
+  
+  # The host name for an Apple Push Notification Server.
+  #
+  # Args:
+  #   server_type:: either :production or :sandbox
+  #   service:: either :push or :feedback
+  def self.apns_host(server_type, service = :push)
+    {
+      :feedback => {
+        :sandbox => 'feedback.sandbox.push.apple.com',
+        :production => 'feedback.push.apple.com'
+      },
+      :push => {
+        :sandbox => 'gateway.sandbox.push.apple.com',
+        :production => 'gateway.push.apple.com'
+      }
+    }[service][server_type]
+  end
+  
+  # The port for an Apple Push Notification Server.
+  #
+  # Args:
+  #   server_type:: either :production or :sandbox
+  #   service:: either :push or :feedback
+  def self.apns_port(server_type, service = :push)
+    {
+      :feedback => 2196,
+      :push => 2195
+    }[service]
+  end
+  
+  # Real implementation of Imobile.push_notifications
+  def self.push_notifications(certificate_or_path, notifications)
+    socket = apns_socket read_certificate(certificate_or_path), :push
+    notifications = [notifications] if notifications.kind_of? Hash
+    notifications.each { |n| socket.write encode_notification(n) }
+    if Kernel.block_given?
+      loop do
+        notifications = yield
+        notifications = [notifications] if notifications.kind_of? Hash
+        notifications.each { |n| socket.write encode_notification(n) }
+      end
+    end
+    socket.close
+  end
+  
+  # Real implementation of Imobile.push_notification
+  def self.push_notification(notification, certificate_or_path)
+    push_notifications certificate_or_path, [notification]
+  end
+
+  # Real implementation of Imobile.push_feedback
+  def self.push_feedback(certificate_or_path, &block)
+    if Kernel.block_given?
+      raw_push_feedback certificate_or_path, &block
+      nil
+    else
+      feedback = []
+      raw_push_feedback certificate_or_path do |feedback_item|
+        feedback << feedback_item
+      end
+      feedback
+    end
+  end
+  
+  # Reads the available feedback from Apple's Push Notification service.
+  #
+  # Args:
+  #   certificate_or_path:: see Imobile.push_notification
+  #
+  # The currently provided feedback is the tokens for the devices which rejected
+  # notifications. Each piece of feedback is a hash with the following keys:
+  #   :device_token:: the device's token, in binary (not hexadecimal) format
+  #   :time:: the last time when the device rejected notifications; according to
+  #           Apple, the rejection can be discarded if the device sent a
+  #           token after this time
+  #
+  # The method reads all the feedback available from the Push Notification
+  # service, and yields each piece of feedback to the method's block.
+  def self.raw_push_feedback(certificate_or_path)
+    socket = apns_socket read_certificate(certificate_or_path), :feedback
+    loop do      
+      break unless header = fixed_socket_read(socket, 6)
+      time = Time.at header[0, 4].unpack('N').first
+      device_token = fixed_socket_read(socket, header[4, 2].unpack('n').first)
+      break unless device_token
+      feedback_item = { :device_token => device_token, :time => time }
+      yield feedback_item
+    end
+    socket.close
+  end
+  
+  # Reads a fixed number of bytes from a socket.
+  def self.fixed_socket_read(socket, num_bytes)
+    data = ''
+    while data.length < num_bytes
+      new_data = socket.read(num_bytes - data.length)
+      return nil if new_data.nil? or new_data.empty?  # Socket closed.
+      data += new_data
+    end
+    data
   end
 end  # module PushNotifications
 
--- test/push_notification_test.rb
+++ test/push_notification_test.rb
@@ -15,26 +15,163 @@ class PushNotificationTest < Test::Unit::TestCase
   def setup
     testdata_path = File.join(File.dirname(__FILE__), '..', 'testdata')
     @dev_cert_path = File.join(testdata_path, 'apns_developer.p12') 
-    @prod_cert_path = File.join(testdata_path, 'apns_production.p12') 
+    @prod_cert_path = File.join(testdata_path, 'apns_production.p12')
+    
+    @hex_dev_token = File.read File.join(testdata_path, 'sandbox_device_token')
+    @dev_token = File.read File.join(testdata_path, 'sandbox_device_token.bin')
+    
+    @notification = {:aps => {:alert => 'imobile test notification'}}
+    @encoded_notification = File.read File.join(testdata_path,
+                                                'encoded_notification')
   end
   
   def test_read_push_certificate
-    cert_data = Imobile::PushNotifications.read_push_certificate @dev_cert_path
+    cert_data = Imobile::PushNotifications.read_certificate @dev_cert_path
     assert cert_data, "Dev certificate didn't load"    
     assert_equal :sandbox, cert_data[:server_type],
                  "Dev certificate mistaken for prod"
     assert cert_data[:key].kind_of?(OpenSSL::PKey::PKey),
            "Dev certificate does not contain a key"
-    assert /Q686F7Z6YU/ =~ cert_data[:certificate].subject.to_s,
-           "Wrong data in dev certificate #{cert_data[:certificate].inspect}"
+    assert(/Q686F7Z6YU/ =~ cert_data[:certificate].subject.to_s,
+           "Wrong data in dev certificate #{cert_data[:certificate].inspect}")
 
-    cert_data = Imobile::PushNotifications.read_push_certificate @prod_cert_path
+    prod_blob = File.read @prod_cert_path
+    cert_data = Imobile::PushNotifications.read_certificate prod_blob 
     assert cert_data, "Prod certificate didn't load"    
     assert_equal :production, cert_data[:server_type],
                  "Prod certificate mistaken for dev"
     assert cert_data[:key].kind_of?(OpenSSL::PKey::PKey),
            "Prod certificate does not contain a key"
-    assert /Q686F7Z6YU/ =~ cert_data[:certificate].subject.to_s,
-           "Wrong data in prod certificate #{cert_data[:certificate].inspect}"
+    assert(/Q686F7Z6YU/ =~ cert_data[:certificate].subject.to_s,
+           "Wrong data in prod certificate #{cert_data[:certificate].inspect}")
+  end
+  
+  def test_pack_device_token
+    assert_equal @dev_token, Imobile.pack_hex_device_token(@hex_dev_token)
+  end
+   
+  def test_encode_notification
+    notification = @notification.merge :device_token => @dev_token
+    encoded = Imobile::PushNotifications.encode_notification notification
+    
+    assert_equal @encoded_notification, encoded
+  end
+  
+  def test_valid_notification
+    assert Imobile.valid_notification?(@notification),
+           'Failed on easy valid notification'
+    notification = @notification.merge :device_token => '1' * 512
+    assert Imobile.valid_notification?(notification),
+           'Failed on notification with large device token'
+    notification[:aps][:alert] = '1' * 512
+    assert !Imobile.valid_notification?(notification),
+           'Passed notification with large alert'
+  end
+  
+  def test_apns_host_port
+    [
+     [:push, @dev_cert_path, 'gateway.sandbox.push.apple.com', 2195],
+     [:push, @prod_cert_path, 'gateway.push.apple.com', 2195],
+     [:feedback, @dev_cert_path, 'feedback.sandbox.push.apple.com', 2196],
+     [:feedback, @prod_cert_path, 'feedback.push.apple.com', 2196]
+    ].each do |service, cert_path, gold_host, gold_port|
+      cert = Imobile::PushNotifications.read_certificate cert_path
+      server_type = cert[:server_type]
+      assert_equal gold_host,
+                   Imobile::PushNotifications.apns_host(server_type, service)
+      assert_equal gold_port,
+                   Imobile::PushNotifications.apns_port(server_type, service)
+    end
+  end
+    
+  def test_smoke_push
+    notification = @notification.merge :device_token => @dev_token
+    Imobile.push_notification notification, @dev_cert_path
+  end
+  
+  def test_feedback_decoding
+    time1 = Time.at((Time.now - 3600).to_i)
+    time2 = Time.at((Time.now - 3600 * 72).to_i)
+    token1 = @dev_token
+    token2 = @dev_token.reverse
+    golden_feedback = [
+      {:device_token => token1, :time => time1},
+      {:device_token => token2, :time => time2}
+    ]
+    
+    reads = [
+      [6, [time1.to_i].pack('N')],
+      [2, [token1.length].pack('n')],
+      [token1.length, token1[0, 10]],
+      [token1.length - 10, token1[10, token1.length - 10]],
+      [6, [time2.to_i].pack('N')[0, 3]],
+      [3, [time2.to_i].pack('N')[3, 1] + [token2.length].pack('n')],
+      [token2.length, token2],
+      [6, '']
+    ]
+    
+    dev_cert = Imobile::PushNotifications.read_certificate @dev_cert_path
+    prod_cert = Imobile::PushNotifications.read_certificate @prod_cert_path
+    socket1, socket2 = MockSocket.new(reads), MockSocket.new(reads)
+    flexmock(Imobile::PushNotifications).should_receive(:apns_socket).
+        with(dev_cert, :feedback).and_return(socket1).once
+    flexmock(Imobile::PushNotifications).should_receive(:apns_socket).
+        with(prod_cert, :feedback).and_return(socket2)
+        
+    # Test no-block codepath.
+    assert_equal golden_feedback, Imobile.push_feedback(dev_cert),
+                 "No-block codepath failed."
+    assert socket1.closed, "No-block codepath didn't close socket"
+    
+    # Test block codepath
+    output_feedback = []
+    Imobile.push_feedback prod_cert do |feedback_item|
+      output_feedback << feedback_item
+    end
+    assert_equal golden_feedback, output_feedback, "Block codepath failed."
+    assert socket2.closed, "Vlock codepath didn't close socket"
+  end
+  
+  def test_smoke_feedback
+    Imobile.push_feedback @dev_cert_path do |feedback_item|
+      check_feedback_item feedback_item
+    end
+    
+    feedback_items = Imobile.push_feedback @prod_cert_path
+    feedback_items.each { |feedback_item| check_feedback_item feedback_item }
+  end
+  
+  # Verifies that a piece of feedback looks valid.
+  def check_feedback_item(feedback)
+    assert feedback[:device_token],
+           "A feedback item does not contain a device token"
+    assert feedback[:time],
+           "A feedback item does not contain a timestamp"
   end  
+end
+
+
+class MockSocket
+  def initialize(expected_reads)
+    @expected_reads = expected_reads.dup
+    @closed = false
+  end
+  attr_reader :closed
+  
+  def read(bytes)
+    expected_read = @expected_reads.shift
+    unless expected_read
+      raise "Unexpected read of #{bytes.inspect} (done sending data)"
+    end
+    
+    unless bytes == expected_read[0]
+      raise "Expected read size #{expected_read[0]}, got #{bytes.inspect}"
+    end
+    expected_read[1]
+  end
+  
+  def close
+    raise "Closed prematurely" unless @expected_reads.empty?
+    @closed = true
+  end
 end
